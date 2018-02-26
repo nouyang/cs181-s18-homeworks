f8dc0aba9cba67952d4d16691b60f3ef20ea4bb5
nouyang <nouyang@email>
2018-02-26 08:26:16 -0500

    problem3.py runs; fixed bug in sigmoid exp(-x), not exp(x)

--- T2/LogisticRegression.py
+++ T2/LogisticRegression.py
@@ -9,44 +9,70 @@ from scipy.misc import logsumexp
 # Bishop, chapter 4.3.4 Multilass logistic regression
 # Bishop, chapter 5.2.4 Gradient descent optimization
 # Bishop, page 10 Regularization 
+
+# Logistic Regression Equation
+# When differentiating loss w.r.t. the weights for class j, we take each
+# datapoint x_i and add the following expression  ( which is the estimated
+# probability of that datapoint x_i  being in class j, minus datapoint x_i's true
+# label (either 0 or 1) for being in class j), which results in a scalar. Then
+# we scape datapoint x_i by that scalar.
+# This results in vector 
 class LogisticRegression:
     def __init__(self, eta, lambda_parameter):
         self.eta = eta
         self.lambda_parameter = lambda_parameter
+        self.weights = np.array(-999) 
+        self.iters = 10
     
     # Just to show how to make 'private' methods
     def __dummyPrivateMethod(self, input):
         return None
 
 
-    def softmax(array_input):
+    def __softmax(self, array_input):
         # Input:
         # - array of score for each class Dims: n.k
         # Output:
         # - vector of normalized scores, normalized by row  Dims: n.k
         exp_data =  np.exp(array_input)
         rowsum = np.sum(exp_data, axis=1)
-        array_softmax = (exp_data/1.0) / sumscore # divide by 1.0 b/c of int division paranoia
+        array_softmax = exp_data.T/rowsum
+        array_softmax = array_softmax.T
         return array_softmax 
-    def err
 
-    def grad_desc(w, x):
+    def __grad_desc(self, x_input, w_input, truec):
         # Input:
-        # -- w - vector of weights
+        # -- w - vector of weights d.k
         # -- x - array of parameters per datapoint
         # Output:
         # - w_new - vector of w' = w - eta*grad(w) - l2 regularization
-        # - class_errs - vector of error for each class
-            w_old = w_new
-            w_reshaped = w.reshape([d, k]) # d.k
-            wdotx = np.multiply(x, w_reshaped.T) # n.k
-            zscores = 1.0 / (1.0 + exp(wdotx)) # sigmoid fxn Dims: n.k
-            softscore = self.softmax(z_scores) # n.k
-            errs = softscores - C # n.k Question: Do we not want the absolute value of y_est - y?
-            class_errs = np.sum(errs, axis=0) # 1.k 
-            regterm = (self.lamda*0.5) * w_old**2  # Question: old w or w + grad(w)? does it matter
-            w_new = w_old - (self.eta*class_errs + regterm) # 1.w
-        return w_new, sum(class_errs)
+        # - loss - ???
+        x = np.copy(x_input)
+        weights = np.copy(w_input)
+        trueclass = np.copy(truec)
+        wdotx = np.dot(x, weights.T) # n.k
+        zscores = 1.0 / (1.0 + np.exp(-wdotx)) # sigmoid fxn Dims: n.k
+        softscores = self.__softmax(zscores) # n.k
+
+        ### TODO PULL OUT --
+        n = x.shape[0] # number of datapoints
+        d = x.shape[1] # number of features per datapoint
+        #k = max(C) # number of classes #Nah... what if our training set is missing a class entirely
+        k = 3
+        ### -- 
+
+        diffs = softscores - trueclass # n.k Question: Do we not want the absolute value of y_est - y? 
+        # A: no, this is a value for calculating the slope of the loss, not the loss itself!!
+        # WRONG: class_errs = np.sum(errs, axis=0) # 1.k 
+        for j in range(k): 
+            gradj = np.zeros(d) #1.d
+            for i in range(n):  #rows
+                xi = x[i,:] #vector  1.d
+                gradj += diffs[i][j] * xi #scalar 1.1 * vector 1.d
+            reg = self.lambda_parameter * np.dot(weights[j,:], weights[j,:]) # ?  
+            reg = 0
+            weights[j,:] = weights[j,:] - (gradj*self.eta + reg) #update step
+        return weights
 
     # todo: document the math behind all of this matrix manipulation
     # Run this before predict to produce a function we can compute on new x
@@ -68,13 +94,13 @@ class LogisticRegression:
         k = 3
         w = k*n # number of weights
 
-        C_hot =np.eye(k)[np.array(C).reshape(-1)] #one-hot. hattip to internet
+        C_hot = np.eye(k)[np.array(self.C).reshape(-1)] #one-hot. hattip to internet
         
-        w_init = ones([1, k*d]) # todo: probably should be random?
-
-        # calc_loss = 
-
-        # run gradient descent
+        weights = np.random.rand( k, d) #init to random
+        for z in range(self.iters):
+            weights = self.__grad_desc(self.X, weights,C_hot) 
+            print(weights)
+        self.weights = weights
         return #predicted weights
 
     # TODO
