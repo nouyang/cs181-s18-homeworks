86815a2f71fff15ff1c02c0a98acd425b2347f26
nouyang <nouyang@email>
2018-03-21 04:35:17 -0400

    Initial kernel perceptron

--- T3/code/problem2.py
+++ T3/code/problem2.py
@@ -7,39 +7,72 @@ from Perceptron import Perceptron
 
 # Implement this class
 class KernelPerceptron(Perceptron):
-	def __init__(self, numsamples):
-		self.numsamples = numsamples
+    def __init__(self, numsamples):
+        self.numsamples = numsamples
+        self.weights = [] #of len(x)
+        self.b = 0.0
 
-	# Implement this!
-	# def fit(self, X, Y):
+    # Implement this!
+    def fit(self, X, Y):
+        # as per https://www.cs.cmu.edu/~avrim/ML10/lect0125.pdf
+        self.X = X
+        self.Y = Y
+        X = np.array(X)
+        self.weights = np.zeros((1, X.shape[1]))
+        self.b = 0.0
+        # normalize X
+        row_sums = X.sum(axis=1) 
+        X = X / row_sums[:, np.newaxis]
+        #rand_idx = np.random.rand(len() # how many random numbers valued b/tw 0 and 1
+        print(len(X))
+        print(len(Y))
+        rand_idx = np.random.permutation(self.numsamples)
+        for i in rand_idx:
+            print(X[i])
+            y_hat = np.dot(self.weights, X[i])
+            print(y_hat)
+            print(Y[i])
+            if Y[i] > y_hat: # 1, -1
+                self.weights += X[i] 
+            if Y[i] < y_hat: # -1, 1
+                self.weights -= X[i]
+            # use sparse natrices
+            # e.g. matrix = {(0, 3): 1, (2, 1): 2, (4, 3): 3}
+            # print(matrix.get((1, 3), 0)) # return 0 if not in dictionary
 
-	# Implement this!
-	# def predict(self, X):
+
+        # Implement this!
+    def predict(self, X):
+        return np.ones(len(X))
+        # return array of 1's and -1's for classifying
 
 # Implement this class
 class BudgetKernelPerceptron(Perceptron):
-	def __init__(self, beta, N, numsamples):
-		self.beta = beta
-		self.N = N
-		self.numsamples = numsamples
-		
-	# Implement this!
-	# def fit(self, X, Y):
+    def __init__(self, beta, N, numsamples):
+        self.beta = beta
+        self.N = Nh
+        self.numsamples = numsamples
 
-	# Implement this!
-	# def predict(self, X):
+        # Implement this!
+    def fit(self, X, Y):
+        pass
+
+    # Implement this!
+    # def predict(self, X):
 
 
 
 # Do not change these three lines.
-data = np.loadtxt("data.csv", delimiter=',')
+#data = np.loadtxt("data.csv", delimiter=',')
+data = np.loadtxt("data_short.csv", delimiter=',')
 X = data[:, :2]
 Y = data[:, 2]
 
 # These are the parameters for the models. Please play with these and note your observations about speed and successful hyperplane formation.
 beta = 0
 N = 100
-numsamples = 20000
+#numsamples = 20000
+numsamples = 100
 
 kernel_file_name = 'k.png'
 budget_kernel_file_name = 'bk.png'
