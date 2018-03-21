b1abd1908d764086666fbe0c74eb9a09bf55e9a1
nouyang <nouyang@email>
2018-03-21 07:33:33 -0400

    redid kernel perceptron as must sum over all support vectors

--- T3/code/problem2.py
+++ T3/code/problem2.py
@@ -11,43 +11,85 @@ from numpy.core.umath_tests import inner1d
 class KernelPerceptron(Perceptron):
     def __init__(self, numsamples):
         self.numsamples = numsamples
-        self.weights = [] #of len(x)
+        self.alphas =  {} #of len(x)
         self.b = 0.0
 
     # Implement this!
     def fit(self, X, Y):
         # as per https://www.cs.cmu.edu/~avrim/ML10/lect0125.pdf
         self.X = X
+        print('asdfsadf', X.shape)
         self.Y = Y
         X = np.array(X)
-        self.weights = np.zeros((1, X.shape[1]))
-        self.b = 0.0
-        # normalize X
-        row_sums = X.sum(axis=1) 
-        X = X / row_sums[:, np.newaxis]
-        #rand_idx = np.random.rand(len() # how many random numbers valued b/tw 0 and 1
-        print(len(X))
-        print(len(Y))
+        # K(x,x2 ) = x.T *  x2
+        np.random.seed(314159)
+        SV_indices = []
+        alphas = {}
+        
+        #b = 0
         rand_idx = np.random.permutation(self.numsamples)
-        for i in rand_idx:
-            print(X[i])
-            y_hat = np.dot(self.weights, X[i])
-            print(y_hat)
-            print(Y[i])
-            if Y[i] > y_hat: # 1, -1
-                self.weights += X[i] 
-            if Y[i] < y_hat: # -1, 1
-                self.weights -= X[i]
-            # use sparse natrices
-            # e.g. matrix = {(0, 3): 1, (2, 1): 2, (4, 3): 3}
-            # print(matrix.get((1, 3), 0)) # return 0 if not in dictionary
+        for t in rand_idx:
+            # kern = np.inner(X[t], X[i])
+            # # all pairs of this t, with all the SV
+            # print(t)
+            # print(SV_indices)
+            # print(X.shape)
+            # print(X[70])
+            y_hat = np.sum(alphas[i]*inner1d(X[t],X[i]) for i in SV_indices)
+            y_true = Y[t]
+            if y_true * y_hat <= 0:
+                SV_indices.append(t)
+                alphas.update({t:y_true})
+        self.alphas = alphas
+
+
+        # self.weights = np.zeros((1, X.shape[1]))
+        # self.b = 0.0
+        # # normalize X
+        # row_sums = X.sum(axis=1) 
+        # X = X / row_sums[:, np.newaxis]
+        # #rand_idx = np.random.rand(len() # how many random numbers valued b/tw 0 and 1
+        # print(len(X))
+        # print(len(Y))
+        # rand_idx = np.random.permutation(self.numsamples)
+        # for i in rand_idx:
+            # print(X[i])
+            # y_hat = np.dot(self.weights, X[i])
+            # print(y_hat)
+            # print(Y[i])
+            # if Y[i] > y_hat: # 1, -1
+                # self.weights += X[i] 
+            # if Y[i] < y_hat: # -1, 1
+                # self.weights -= X[i]
+            # # use sparse natrices
+            # # e.g. matrix = {(0, 3): 1, (2, 1): 2, (4, 3): 3}
+            # # print(matrix.get((1, 3), 0)) # return 0 if not in dictionary
 
 
         # Implement this!
     def predict(self, X):
-        Y = inner1d(self.weights,np.array(X))
-        print(Y.shape)
-        return Y
+        print("========= *** Predicting *** ========")
+
+        X = np.array(X)
+        print(X.shape)
+        kern = inner1d(X, X)
+        print(X.shape)
+        print(kern.shape)
+        print(self.alphas)
+        # self.weights, and 0 if not in self.weights
+        n = X.shape[0] 
+        weights = np.zeros(n)
+        print('wegiths shape', weights.shape)
+        dict = self.alphas
+        for k,v in dict.items():
+            weights[k] = v
+        print(weights.shape, kern.shape)
+        print(kern[0:12])
+        print(weights[0:12])
+        y_hat = np.multiply(weights, kern)
+        print(kern.shape)
+        print('yhat ', y_hat.shape)
+        return y_hat
 
         # return array of 1's and -1's for classifying
 
@@ -77,7 +119,7 @@ Y = data[:, 2]
 beta = 0
 N = 100
 #numsamples = 20000
-numsamples = 2000
+numsamples = 100
 
 kernel_file_name = 'k.png'
 budget_kernel_file_name = 'bk.png'
