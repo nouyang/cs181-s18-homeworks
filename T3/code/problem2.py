a2b0f6eed7e003e0ee5b2249bfd6e13ae75897ad
nouyang <nouyang@email>
2018-03-22 13:40:06 -0400

    working budget perceptron

--- T3/code/problem2.py
+++ T3/code/problem2.py
@@ -3,6 +3,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.colors as c
+import time
 from Perceptron import Perceptron
 from numpy.core.umath_tests import inner1d
 
@@ -17,16 +18,16 @@ class KernelPerceptron(Perceptron):
         self.X = []
         self.Y = []
 
-    # Implement this!
     def fit(self, X, Y):
+        print("\n\n========= *** Fitting Perceptron *** ==========")
+        t_start = time.time()
         X = np.array(X)
         self.X = X
         self.Y = Y
         # K(x,x2 ) = x.T *  x2
-        np.random.seed(314159)
+        #np.random.seed(314159)
         SV_indices = set()
         alphas = {}
-        print('X shape', X.shape)
         
         for i in range(self.numsamples):
             t = np.random.randint(X.shape[0])
@@ -38,29 +39,28 @@ class KernelPerceptron(Perceptron):
                 alphas.update({t:y_true})
         self.alphas = alphas
         self.SV_indices = SV_indices
-        print('indices', SV_indices)
-        print('alphas', alphas)
+        t_elapsed = time.time() - t_start
+        print("Elapsed time for fitting ", X.shape, " datapoints: ", t_elapsed, " seconds." )
 
     def predict(self, X):
+        t_start = time.time()
         print("========= *** Predicting *** ==========")
-        print('x shape', X.shape)
-        print('x shape', self.X.shape)
         print('support vector indices', self.SV_indices)
-        print('alphas', self.alphas)
+        print('\nalphas', self.alphas)
 
         y_hats = []
         for xt in X:
             boo =  [self.alphas[sv_i]*np.dot(xt,self.X[sv_i]) for sv_i in self.SV_indices] 
-            print('boo', boo)
+            #print('boo', boo)
             y_hat = np.sum(boo)
+            if y_hat == 0: #dealing with the meshgrid has zeros at the beginning
+                y_hat = 1
             y_hats.append(y_hat)
         y_hats = np.array(y_hats)
         y_hats = np.sign(y_hats)
 
-        print(y_hats.shape)
-        print('y_hats', y_hats)
-        print('alphas', self.alphas)
-
+        t_elapsed = time.time() - t_start
+        print("Elapsed time for predicting", X.shape, " datapoints: ", t_elapsed, "seconds.")
         return y_hats 
 
 
@@ -70,47 +70,85 @@ class BudgetKernelPerceptron(Perceptron):
         self.beta = beta
         self.N = N
         self.numsamples = numsamples
+        self.X = X
+        self.Y = Y
 
-    # Implement this!
-    # TODO: after kernel perceptron is fixed, merge into budget perceptron
     def fit(self, X, Y):
-        # update step
-        if y_true * y_hat <= self.beta:
-            SV_indices.append(t)
-            alphas.update({t:y_true})
-            # removal step
-            if len(SV_indices) > self.N:
-                foo = [ Y[i] * (y_hat[i] - alphas[i]*np.dot(X[i].T, x[i])) \
-                        for i in SV_indices ]
-                arg_maxmarg = foo.index(max(foo))
-                SV_indices.pop(arg_maxmarg)
+        print("\n\n========= *** Fitting Budget Perceptron *** ==========")
+        t_start = time.time()
 
+        X = np.array(X)
+        self.X = X
+        self.Y = Y
 
-    # Implement this!
-    # def predict(self, X):
+        SV_indices = []
+        alphas = {}
+        y_hats = {}
 
+        for i in range(self.numsamples):
+            t = np.random.randint(X.shape[0])
+            xt = X[t]
+            y_hat = np.sum(alphas[idx]*inner1d(xt,X[idx]) for idx in SV_indices)
+            y_hats.update({t:y_hat})
+            y_true = Y[t]
+
+            if y_true * y_hat <= self.beta:
+                if t not in SV_indices:
+                    SV_indices.append(t)
+                alphas.update({t:y_true})
+                # removal step
+                if len(SV_indices) > self.N:
+                    foo = [ Y[i] * (y_hats[i] - alphas[i]*np.dot(X[i], X[i])) \
+                            for i in SV_indices ]
+                    arg_maxmarg = foo.index(max(foo))
+                    #print('remove', arg_maxmarg)
+                    #print('sV_indices', SV_indices)
+                    del(SV_indices[arg_maxmarg])
+        self.alphas = alphas
+        self.SV_indices = SV_indices
+        t_elapsed = time.time() - t_start
+        print("Elapsed time for fitting ", X.shape, " datapoints: ", t_elapsed, " seconds." )
+
+    def predict(self, X):
+        t_start = time.time()
+        print("========= *** Predicting *** ==========")
+        #print('support vector indices', self.SV_indices)
+        #print('alphas', self.alphas)
+
+        y_hats = []
+        for xt in X:
+            y_hat = np.sum(boo)
+            if y_hat == 0: #dealing with the meshgrid has zeros at the beginning
+                y_hat = 1
+                print('\nY_hat is zero. boo: ', boo, ' xt: ', xt)
+            y_hats.append(y_hat)
+        y_hats = np.array(y_hats)
+        y_hats = np.sign(y_hats)
+
+        t_elapsed = time.time() - t_start
+        print("Elapsed time for predicting", X.shape, " datapoints: ", t_elapsed, "seconds.")
+        return y_hats 
 
 
 # Do not change these three lines.
-#data = np.loadtxt("data.csv", delimiter=',')
-data = np.loadtxt("data_short.csv", delimiter=',')
+data = np.loadtxt("data.csv", delimiter=',')
+#data = np.loadtxt("data_short.csv", delimiter=',')
 X = data[:, :2]
 Y = data[:, 2]
 
 # These are the parameters for the models. Please play with these and note your observations about speed and successful hyperplane formation.
 beta = 0 #budget
 N = 100 #budget
-#numsamples = 20000
-numsamples = 200
+numsamples = 20000
+#numsamples = 200
 
 kernel_file_name = 'k.png'
 budget_kernel_file_name = 'bk.png'
 
 # Don't change things below this in your final version. Note that you can use the parameters above to generate multiple graphs if you want to include them in your writeup.
 k = KernelPerceptron(numsamples)
-k.fit(X,Y)
-k.visualize(kernel_file_name, width=0, show_charts=True, save_fig=True, \
-        include_points=True)
+#k.fit(X,Y)
+#k.visualize(kernel_file_name, width=0, show_charts=True, save_fig=True, include_points=True)
 
 bk = BudgetKernelPerceptron(beta, N, numsamples)
 bk.fit(X, Y)
