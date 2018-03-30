f3eac2655993deebcea88c5e02344668085a6021
nouyang <nouyang@email>
2018-03-30 06:43:26 -0400

    working on 3x3 'images', but code fails if all 'images' assigned to one cluster and there ais an empty cluster

--- T4/problem3.py
+++ T4/problem3.py
@@ -13,48 +13,60 @@ class KMeans(object):
     # EDIT: X is flattened to N.(28^2)
     def __init__(self, K):
         self.K = K
-        self.dim = 28
+        self.dim = -1
         self.X = []
         self.numImages = -1
     #np.random.seed(314159)
 
-    def dist(a,b):
+    def dist(self, a,b):
         return np.linalg.norm(a-b) #l2 norm
 
     def closestCenters(self, imgs, centers):
-        print(centers)
+        #print(centers)
         # we shall have something like [0,4,3,5,1,0,0] for each point, their cluster
-        for img in range(imgs.shape[0]):
+        imgAssignments = []
+        for img in imgs: 
+            #print('img\n', img)
             minDist, k = 99999999, -999
-            for kIndex in range(centers.shape[0]):
+            for kIndex in range(self.K):
                 center = centers[kIndex]
-                distortion = dist(img, center)
+                distortion = self.dist(img, center)
                 if distortion < minDist:
                     minDist = distortion
                     k = kIndex
-            imgAssignments[x] = k
-        return pointAssignments
+            imgAssignments.append(k)
+        return imgAssignments
 
     # we have an array centers = [[gray1, gray2, ... 28^2] ... K]], for mean image of the K clusters
     def newCenters(self, imgs, clusterAssignments):
         #clusters = np.zeros(self.X.shape[0], self.dim**2)
         centers = []
-        for k in range(self.K): #retrieve points that are for each cluster
+        print('clusterAssignments\n', clusterAssignments)
+        for k in range(self.K): #retrieve imgs that are for each cluster
             cluster = np.array([imgs[i] for i in range(self.numImages) \
                                 if clusterAssignments[i]==k])
+            print('k=', k, 'cluster\n', cluster)
             centers.append(np.mean(cluster, axis=0))
+        print('centers\n', centers)
         return np.array(centers)
 
     def fit(self, X):
+        self.dim = X.shape[1]
         K = self.K
         self.numImages = X.shape[0]
         self.X = X.reshape(self.numImages, -1) #flatten into 2D N.(28*28) array
-        currCenters = np.dstack((np.random.rand(K), np.random.rand(K)))
+        #currCenters = [np.random.rand(K) for k in range(self.K)]
+        currCenters = np.random.randint(0,10,(self.numImages, self.dim, self.dim))
+        print('hi')
         print(currCenters)
-        print(currCenters.shape)
-        for i in range(1,100):
+        #print(currCenters)
+        #print(currCenters.shape)
+        numIters = 5
+        for i in range(numIters):
+            print('now assigning points to clusters!!')
             clusterAssignments = self.closestCenters(X, currCenters)
-            currCenters = newCenters(X, clusterAssignments)
+            print('now calculating new centers!!')
+            currCenters = self.newCenters(X, clusterAssignments)
         return clusterAssignments, currCenters
 
     # This should return the arrays for K images. Each image should represent the mean of each of the fitted clusters.
@@ -76,6 +88,7 @@ class KMeans(object):
 
 # This line loads the images for you. Don't change it! 
 pics = np.load("images.npy", allow_pickle=False)
+#print(pics[0])
 
 # You are welcome to change anything below this line. This is just an example of how your code may look.
 # That being said, keep in mind that you should not change the constructor for the KMeans class, 
@@ -85,9 +98,14 @@ K = 10
 # what is useKMeansPP?
 #https://www.rdocumentation.org/packages/pracma/versions/1.5.5/topics/kmeanspp
 #KMeansClassifier = KMeans(K=10, useKMeansPP=False)
-KMeansClassifier = KMeans(K=10)
-KMeansClassifier.fit(pics)
-KMeansClassifier.create_image_from_array(pics[1])
+imgs = np.array([[num]*9 for num in range(5)]).reshape(5,3,3)
+
+KMeansClassifier = KMeans(K=2)
+KMeansClassifier.fit(imgs)
+#KMeansClassifier = KMeans(K=10)
+#KMeansClassifier.fit(pics)
+
+#KMeansClassifier.create_image_from_array(pics[1])
 
 
 
