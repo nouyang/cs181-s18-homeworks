7c5d97662ddec685de48063d7c951f0e46d87969
nouyang <nouyang@email>
2018-03-30 07:00:59 -0400

    added mean image retrieval, which seems to work

--- T4/problem3.py
+++ T4/problem3.py
@@ -16,6 +16,7 @@ class KMeans(object):
         self.dim = -1
         self.X = []
         self.numImages = -1
+        self.centers = []
         np.random.seed(314159)
 
     def dist(self, a,b):
@@ -41,41 +42,44 @@ class KMeans(object):
     def newCenters(self, imgs, clusterAssignments):
         #clusters = np.zeros(self.X.shape[0], self.dim**2)
         centers = []
-        print('clusterAssignments\n', clusterAssignments)
+        #print('clusterAssignments\n', clusterAssignments)
+        print('clusterAssignments\n', clusterAssignments[0:10])
         for k in range(self.K): #retrieve imgs that are for each cluster
             cluster = np.array([imgs[i] for i in range(self.numImages) \
                                 if clusterAssignments[i]==k])
             if cluster.size:
-                print('k=', k, 'cluster\n', cluster)
+                #print('k=', k, 'cluster\n', cluster)
                 centers.append(np.mean(cluster, axis=0))
             else:
                 # randomly reinitalize center
+                print('WARNING: Had to randomly reinitialize a center!')
                 centers.append( np.random.randint(0,10,(self.dim, self.dim)))
-        print('centers\n', centers)
+        #print('centers\n', centers)
+        self.centers = centers
         return np.array(centers)
 
     def fit(self, X):
         self.dim = X.shape[1]
         K = self.K
         self.numImages = X.shape[0]
+        print('self.numImages', self.numImages)
         self.X = X.reshape(self.numImages, -1) #flatten into 2D N.(28*28) array
         #currCenters = [np.random.rand(K) for k in range(self.K)]
         currCenters = np.random.randint(0,10,(self.numImages, self.dim, self.dim))
-        print('hi')
-        print(currCenters)
         #print(currCenters)
-        #print(currCenters.shape)
-        numIters = 5
+        numIters = 1
         for i in range(numIters):
-            print('now assigning points to clusters!!')
+            print('i_th iteration', i)
+            #print('now assigning points to clusters!!')
             clusterAssignments = self.closestCenters(X, currCenters)
-            print('now calculating new centers!!')
+            #print('now calculating new centers!!')
             currCenters = self.newCenters(X, clusterAssignments)
         return clusterAssignments, currCenters
 
     # This should return the arrays for K images. Each image should represent the mean of each of the fitted clusters.
     def get_mean_images(self):
-        pass
+        print('shape of centers', np.array(self.centers).shape)
+        return self.centers
 
     # This should return the arrays for D images from each cluster that are representative of the clusters.
     def get_representative_images(self, D):
@@ -99,17 +103,19 @@ pics = np.load("images.npy", allow_pickle=False)
 # though you may add more public methods for things like the visualization if you want.
 # Also, you must cluster all of the images in the provided dataset, so your code should be fast enough to do that.
 K = 10
-# what is useKMeansPP?
+
+# what is useKMeansPP? let us ignore it for now
 #https://www.rdocumentation.org/packages/pracma/versions/1.5.5/topics/kmeanspp
 #KMeansClassifier = KMeans(K=10, useKMeansPP=False)
-imgs = np.array([[num]*9 for num in range(5)]).reshape(5,3,3)
 
-KMeansClassifier = KMeans(K=3)
-KMeansClassifier.fit(imgs)
-#KMeansClassifier = KMeans(K=10)
-#KMeansClassifier.fit(pics)
+#imgs = np.array([[num]*9 for num in range(5)]).reshape(5,3,3)
+#KMeansClassifier = KMeans(K=3)
+#KMeansClassifier.fit(imgs)
 
-#KMeansClassifier.create_image_from_array(pics[1])
+KMeansClassifier = KMeans(K=10)
+KMeansClassifier.fit(pics)
+blah = KMeansClassifier.get_mean_images()
+KMeansClassifier.create_image_from_array(blah[1])
 
 
 
