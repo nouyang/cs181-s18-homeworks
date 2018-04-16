cd102069053ee80642047131e3f28411f5cc7248
nouyang <nouyang@email>
2018-04-16 01:54:40 -0400

    questions 1 and 2

---
+++ T5/kalman.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on  15 Apr
+
+@author: nrw 
+"""
+
+import numpy as np
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+df = pd.read_csv('./kf-data.csv' )
+print(list(df))
+print(df.head())
+
+# Get x and y
+z = df['z'].values.tolist()
+x = df['x'].values.tolist()
+t = df['Unnamed: 0'].values.tolist()
+x[10] = 10.2
+
+#multiplying two gaussians
+def gauss_update(mean1, var1, mean2, var2):
+    new_mean = (var2*mean1 + var1*mean2) / (var2 + var1)
+    new_var = 1 / ((1/var1)+ (1/var2))
+    return new_mean, new_var
+
+def update(likelihood_mean, likelihood_var, prior_mean, prior_var):
+    posterior_mean, posterior_var = gauss_update(
+        likelihood_mean, likelihood_var, prior_mean, prior_var)
+    return (posterior_mean, posterior_var)
+
+def predict(prior_mean, prior_var, motion_mean, motion_var):
+    post_mean = prior_mean + motion_mean
+    post_var = prior_var + motion_var
+    return post_mean, post_var
+
+
+# aka z kind of dithers around the same point 
+# and x is a noisy measurement of z 
+mean_eps = 0
+var_eps = 0.05**2
+mean_y = 0
+var_y = 1**2
+
+mean_prior = 5
+var_prior = 1**2
+
+post_mean = mean_prior
+post_var = var_prior 
+
+zs = []
+zs_var = []
+errs = []
+for step in t:
+    # update with new measurement ll
+    meas = x[step]
+    meas_mean = meas
+    meas_var = var_y
+
+    mean, var = update(meas_mean, meas_var, post_mean, post_var)
+    mean, var = predict(mean, var, mean_eps, var_eps)
+    post_mean, post_var = mean, var
+    est_z = 0 #np.random.normal(post_mean, post_var)
+
+    zs.append(post_mean)
+    zs_var.append(post_var)
+    print("measured %0.02f, post mean %0.02f, post var %0.02f, z_est %.02f, true_z %0.02f" % \
+          (meas, post_mean, post_var, est_z, z[step]))
+
+e = np.array(zs_var)*2
+plt.errorbar(t, zs, e, label='est z',linewidth=1, marker='.', capsize=3)
+#plt.plot(t, zs)
+plt.plot(t, z, label='true z')
+# plt.plot(t, x, label='measurement')
+plt.legend()
+plt.show()
+
+
+
+# Hi,
+# We are referring to the effect that 'incorrect' parameters would have. So, if the given parameters
+# μϵ/γ/p, σϵ/γ/p are different from the original parameters that were used to generate the data, how
+# would your predictions about the hidden state trajectory get affected as t goes from 1 to T.
+# Hope this helps!
+
