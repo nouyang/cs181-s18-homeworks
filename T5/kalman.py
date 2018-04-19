9743dc70d2dc128cf0bf66b3541e2d4e5a7aaab3
nouyang <nouyang@email>
2018-04-18 22:09:49 -0400

    pictures

--- T5/kalman.py
+++ T5/kalman.py
@@ -28,6 +28,8 @@ def gauss_update(mean1, var1, mean2, var2):
     new_var = 1 / ((1/var1)+ (1/var2))
     return new_mean, new_var
 
+
+# here likelihood = our measurement
 def update(likelihood_mean, likelihood_var, prior_mean, prior_var):
     posterior_mean, posterior_var = gauss_update(
         likelihood_mean, likelihood_var, prior_mean, prior_var)
@@ -39,18 +41,18 @@ def predict(prior_mean, prior_var, motion_mean, motion_var):
     return post_mean, post_var
 
 
-# aka z kind of dithers around the same point 
-# and x is a noisy measurement of z 
+# aka z moves by kind of dithering around the same point (epsilon)
+# and x is a noisy measurement of z (gamma)
 mean_eps = 0
 var_eps = 0.05**2
+
 mean_y = 0
 var_y = 1**2
+meas_var = var_y #constant
 
-mean_prior = 5
-var_prior = 1**2
+prior_mean = 5
+prior_var = 1**2
 
-post_mean = mean_prior
-post_var = var_prior 
 
 zs = []
 zs_var = []
@@ -59,24 +61,28 @@ for step in t:
     # update with new measurement ll
     meas = x[step]
     meas_mean = meas
-    meas_var = var_y
+# our current estimate is mean, var
+    mean, var = update(meas_mean, meas_var, prior_mean, prior_var) #measurement
+    mean, var = predict(mean, var, mean_eps, var_eps) #motion
+# let our current estimate by the prior for the next step
+    prior_mean, prior_var = mean, var
 
-    mean, var = update(meas_mean, meas_var, post_mean, post_var)
-    mean, var = predict(mean, var, mean_eps, var_eps)
-    post_mean, post_var = mean, var
-    est_z = 0 #np.random.normal(post_mean, post_var)
+    zs.append(mean)
+    zs_var.append(var)
 
-    zs.append(post_mean)
-    zs_var.append(post_var)
-    print("measured %0.02f, post mean %0.02f, post var %0.02f, z_est %.02f, true_z %0.02f" % \
-          (meas, post_mean, post_var, est_z, z[step]))
+    print("measured %0.02f, curr est mean %0.02f, curr est var %0.02f, true_z %0.02f" % \
+          (meas, mean, var, z[step]))
 
-e = np.array(zs_var)*2
-plt.errorbar(t, zs, e, label='est z',linewidth=1, marker='.', capsize=3)
+e = np.array(np.sqrt(zs_var))*2
+plt.errorbar(t, zs, e, label='est (mean of) z',linewidth=1, marker='.', capsize=3)
+plt.plot(t, x, label='measurement')
 #plt.plot(t, zs)
-plt.plot(t, z, label='true z')
+plt.plot(t, z, label='true z (by oracle)')
 # plt.plot(t, x, label='measurement')
 plt.legend()
+plt.title("Kalman Filter 1D with bad sample")
+plt.xlabel("Time step")
+plt.ylabel("Kalman")
 plt.show()
 
 
@@ -86,4 +92,15 @@ plt.show()
 # μϵ/γ/p, σϵ/γ/p are different from the original parameters that were used to generate the data, how
 # would your predictions about the hidden state trajectory get affected as t goes from 1 to T.
 # Hope this helps!
+
+
+# Since we think our hidden variable cannot move too far, and we think our measurements are not that
+# far off. Thus, when we get a measurement of 3.56 and then a measurement of 10.2, and our
+# measurement has settled around 3.5 with only a small variance (0.1), we cannot update our estimate
+# of the hidden variable to 10. Instead, we can assume our prior estimate of z is at most (that
+# small measurment variance) off, and then the most z can move is on the order of epsilon, and thus
+# we update our estimate to be 4.1. Furthermore, since we are fairly certain in our estimates (as
+# limited by our initial measurement variance tempered by the mostly consistent samples we've had so
+# far), our z estimate will only shift so far on each time step (with a reasonable measurement).
+# Thus, at the end of time, our mean still has not recovered to be close to the true z.
 
